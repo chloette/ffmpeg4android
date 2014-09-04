1af4b69c0e92047984a56a2155dbee8183f473cc
E <chloette.e@gmail.com>
2014-09-04 20:18:51 +0800

    enable all encoders and X264.

--- libavcodec/g723_1.d
+++ libavcodec/g723_1.d
@@ -17,4 +17,4 @@ libavcodec/g723_1.o: libavcodec/g723_1.c libavutil/channel_layout.h \
  libavutil/avassert.h libavcodec/mathops.h libavcodec/arm/mathops.h \
  libavcodec/acelp_vectors.h libavcodec/celp_filters.h \
  libavcodec/celp_math.h libavcodec/g723_1_data.h libavcodec/internal.h \
- libavutil/mathematics.h
+ libavutil/mathematics.h libavcodec/put_bits.h
