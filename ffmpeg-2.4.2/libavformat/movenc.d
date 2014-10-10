c5e45f92ea31d9b8971b4219f91cd93c95921cad
E <chloette.e@gmail.com>
2014-10-10 12:27:53 +0800

    add new branch: 2.4.2

---
+++ ffmpeg-2.4.2/libavformat/movenc.d
@@ -0,0 +1,26 @@
+libavformat/movenc.o: libavformat/movenc.c libavformat/movenc.h \
+ libavformat/avformat.h libavcodec/avcodec.h libavutil/samplefmt.h \
+ libavutil/avutil.h libavutil/common.h libavutil/attributes.h \
+ libavutil/version.h libavutil/macros.h libavutil/avconfig.h config.h \
+ libavutil/intmath.h libavutil/arm/intmath.h libavutil/attributes.h \
+ libavutil/mem.h libavutil/error.h libavutil/internal.h libavutil/timer.h \
+ libavutil/log.h libavutil/arm/timer.h libavutil/cpu.h libavutil/dict.h \
+ libavutil/pixfmt.h libavutil/old_pix_fmts.h libavutil/libm.h \
+ libavutil/intfloat.h libavutil/rational.h libavutil/mathematics.h \
+ libavutil/avutil.h libavutil/buffer.h libavutil/cpu.h \
+ libavutil/channel_layout.h libavutil/dict.h libavutil/frame.h \
+ libavutil/buffer.h libavutil/samplefmt.h libavutil/log.h \
+ libavutil/pixfmt.h libavutil/rational.h libavcodec/version.h \
+ libavutil/version.h libavcodec/old_codec_ids.h libavformat/avio.h \
+ libavutil/common.h libavformat/version.h libavformat/avio_internal.h \
+ libavformat/url.h libavformat/riff.h libavformat/internal.h \
+ libavformat/metadata.h libavformat/isom.h libavformat/dv.h \
+ libavformat/avc.h libavcodec/get_bits.h libavutil/intreadwrite.h \
+ libavutil/bswap.h libavutil/arm/bswap.h libavutil/arm/intreadwrite.h \
+ libavutil/avassert.h libavcodec/mathops.h libavcodec/arm/mathops.h \
+ libavcodec/put_bits.h libavcodec/vc1_common.h libavcodec/raw.h \
+ libavcodec/avcodec.h libavutil/internal.h libavutil/avstring.h \
+ libavutil/intfloat.h libavutil/mathematics.h libavutil/opt.h \
+ libavutil/pixdesc.h libavutil/timecode.h libavformat/hevc.h \
+ libavformat/rtpenc.h libavformat/rtp.h libavformat/avformat.h \
+ libavformat/mov_chan.h
